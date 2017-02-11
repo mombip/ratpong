076d6c3d9f0f49c6e4e1e1f5a19f4655ec89fdf1
michaljaszczyk <vlad.ldz@gmail.com>
2017-02-11 13:15:06 +0100

    ScroreRepositoryInMem first try

--- src/main/java/pl/setblack/pongi/scores/repo/ScoresRepositoryInMem.java
+++ src/main/java/pl/setblack/pongi/scores/repo/ScoresRepositoryInMem.java
@@ -1,36 +1,49 @@
 package pl.setblack.pongi.scores.repo;
 
+import com.google.common.collect.Lists;
 import javaslang.collection.HashMap;
 import javaslang.collection.List;
 import javaslang.collection.PriorityQueue;
 import javaslang.control.Option;
 import pl.setblack.pongi.scores.ScoreRecord;
 import pl.setblack.pongi.scores.UserScore;
+import pl.setblack.pongi.users.repo.UserData;
 
-import java.util.Comparator;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ScoresRepositoryInMem implements ScoresRepository{
 
+    private volatile HashMap<String, UserScore> allScores = HashMap.empty();
 
     public ScoresRepositoryInMem() {
 
     }
 
-
     @Override
     public void registerScore(List<ScoreRecord> rec) {
-        throw new UnsupportedOperationException();
+        for(ScoreRecord score : rec) {
+            UserScore userScore = allScores.getOrElse(score.userId, UserScore.emptyFor(score.userId)).add(score);
+            allScores = allScores.put(score.userId, userScore);
+        }
     }
-
 
     @Override
     public Option<UserScore> getUserScore(String userId) {
-        throw new UnsupportedOperationException();
+        return allScores.get(userId);
     }
 
     @Override
     public List<UserScore> getTopScores(int limit) {
-        throw new UnsupportedOperationException();
+        Map<String, UserScore> result = new LinkedHashMap<>();
+        return allScores.toJavaMap().entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.comparingInt(o -> o.totalScore)))
+                .map(e -> e.getValue())
+                .collect(Collectors.toList());
+
+        return result.entrySet()
+                .stream()
+                .limit(limit));
     }
 
 
